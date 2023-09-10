4b7a3763918fd9666ad49a25705cf8355ec9905a
shilob123 <144595449+shilob123@users.noreply.github.com>
2023-09-10 13:55:36 +0300

    Create if-else.java

---
+++ if-else.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Scanner console = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        double x = console.nextDouble();
+        
+        System.out.println("Please enter another number: "); // No need to create a new Scanner object
+        double y = console.nextDouble();
+        
+        System.out.println("Please enter another number: "); // No need to create a new Scanner object
+        double t = console.nextDouble();
+        
+        if (x < t && x < y) {
+            System.out.println(x);
+        }
+        if (x > t && t < y) {
+            System.out.println(t);
+        }
+        else
+        System.out.println(y);
+        */
+        /*
+         Scanner console = new Scanner(System.in);
+        
+        System.out.println("Please enter the first x coordinate: ");
+        double x1 = console.nextDouble();
+        
+        System.out.println("Please enter the second x coordinate: ");
+        double x2 = console.nextDouble();
+        
+        System.out.println("Please enter the first y coordinate: ");
+        double y1 = console.nextDouble();
+        
+        System.out.println("Please enter the second y coordinate: ");
+        double y2 = console.nextDouble();
+        
+        double x3 = Math.pow(x1 - x2, 2);
+        double y3 = Math.pow(y1 - y2, 2);
+        
+        double middle = Math.sqrt(x3 + y3);
+        
+        System.out.println("The distance between the two points is: " + middle);
+        */
+        
+        /*
+        Scanner console = new Scanner(System.in);
+        
+        System.out.println("Please enter number ");
+        double x = console.nextDouble();
+        
+        if (x>0){
+            System.out.println("positive");
+        }
+        else
+        System.out.println("negative");
+    */
+        
+        Scanner console = new Scanner(System.in);
+        String x;
+        String y;
+        
+        System.out.println("Please enter a char ");
+        x = console.next();
+        
+        System.out.println("Please enter a char ");
+        y = console.next();
+        
+        System.out.println("Please enter a char ");
+        x1 = console.next();
+        
+        System.out.println("Please enter a char ");
+        y1 = console.next();
+        
+        
+    
+    
+    }
+    
+    
+}
